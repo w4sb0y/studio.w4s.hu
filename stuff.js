24da7bc9b25179531d1ffc064dd3693949b9ba40
w4sb0y <68382177+w4sb0y@users.noreply.github.com>
2023-08-09 15:46:33 +0300

    Update stuff.js

--- stuff.js
+++ stuff.js
@@ -6,6 +6,11 @@ app.get('/image', (req, res) => {
   res.sendFile(__dirname + '/w4sstudio.svg');
 });
 
+app.get('/image', (req, res) => {
+  // Serve the specific image for /image route
+  res.sendFile(__dirname + '/itt-a-kurva-svgd.svg');
+});
+
 // Serve the main website for all other routes
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
